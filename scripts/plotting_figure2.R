75a8b4aa9823f9aa9301e788cb892eb71a15de36
Florencia Schlamp <florschlamp@gmail.com>
2021-03-08 18:09:22 -0500

    Add files via upload

---
+++ scripts/plotting_figure2.R
@@ -0,0 +1,221 @@
+# Scripts to generate plots in Figure 2
+# last updated: March 4th, 2021
+# Florencia Schlamp
+# mfs97@cornell.edu
+
+library(ggplot2)
+
+# Figure 2B and 2C) Plot smoothing vs normalized counts
+
+# read in data:
+
+smooth_short_A <- read.csv("../results/top_genes_limma-voom_short.filtered_A_spline_3_TMM_ALL_SMOOTHED.csv",
+                           header=TRUE, row.names=1, as.is=TRUE, check.names=FALSE, sep=',')
+smooth_short_B <- read.csv("../results/top_genes_limma-voom_short.filtered_B_spline_3_TMM_ALL_SMOOTHED.csv",
+                           header=TRUE, row.names=1, as.is=TRUE, check.names=FALSE, sep=',')
+smooth_short_1to7_A <- read.csv("../results/top_genes_limma-voom_short.filtered_A_spline_3_TMM_1to7_ALL_SMOOTHED.csv",
+                                header=TRUE, row.names=1, as.is=TRUE, check.names=FALSE, sep=',')
+smooth_short_1to7_B <- read.csv("../results/top_genes_limma-voom_short.filtered_B_spline_3_TMM_1to7_ALL_SMOOTHED.csv",
+                                header=TRUE, row.names=1, as.is=TRUE, check.names=FALSE, sep=',')
+
+dat_norm <- read.table('../data/normalized_counts_log_filt.csv',
+                       header=TRUE, row.names=1, as.is=TRUE, 
+                       check.names=FALSE, sep=',')
+dat_norm_short <- subset(dat_norm, select = -c(4,19,20,21,39,40,41))
+
+sample_table <- read.csv("../data/metadata_table.csv",header=TRUE)
+sample_table_short <- sample_table[-c(4,19,20,21,39,40,41),]
+
+load("../data/translate_geneID_genename.rdata")
+
+
+# Figure 2B) smoothing of short term patterns
+# example with Gale and Galk
+
+genes <- c("Gale","Galk")
+
+list_of_interest <- translation_file_filtered[translation_file_filtered$gene_symbol %in% genes,]$geneID
+type = 'name'
+
+genes_from_time_course <- dat_norm_short # choose if dat_norm or dat_norm_short
+start_table <- sample_table_short # choose if sample_table or sample_table_short
+
+genes_from_time_course_filt <- genes_from_time_course[list_of_interest,]
+smooth_short_A_filt <- smooth_short_A[list_of_interest,]
+smooth_short_B_filt <- smooth_short_B[list_of_interest,]
+smooth_short_1to7_A_filt <- smooth_short_1to7_A[list_of_interest,]
+smooth_short_1to7_B_filt <- smooth_short_1to7_B[list_of_interest,]
+
+rownames(genes_from_time_course_filt) <- as.character(plyr::mapvalues(x=rownames(genes_from_time_course_filt),
+                                                                      from=translation_file_filtered$geneID,
+                                                                      to=translation_file_filtered$gene_symbol,
+                                                                      warn_missing = F))
+rownames(smooth_short_A_filt) <- as.character(plyr::mapvalues(x=rownames(smooth_short_A_filt),
+                                                              from=translation_file_filtered$geneID,
+                                                              to=translation_file_filtered$gene_symbol,
+                                                              warn_missing = F))
+rownames(smooth_short_B_filt) <- as.character(plyr::mapvalues(x=rownames(smooth_short_B_filt),
+                                                              from=translation_file_filtered$geneID,
+                                                              to=translation_file_filtered$gene_symbol,
+                                                              warn_missing = F))
+rownames(smooth_short_1to7_A_filt) <- as.character(plyr::mapvalues(x=rownames(smooth_short_1to7_A_filt),
+                                                                   from=translation_file_filtered$geneID,
+                                                                   to=translation_file_filtered$gene_symbol,
+                                                                   warn_missing = F))
+rownames(smooth_short_1to7_B_filt) <- as.character(plyr::mapvalues(x=rownames(smooth_short_1to7_B_filt),
+                                                                   from=translation_file_filtered$geneID,
+                                                                   to=translation_file_filtered$gene_symbol,
+                                                                   warn_missing = F))
+
+
+datalist = list()
+
+for(word in genes){
+  if(word %in% rownames(genes_from_time_course_filt)){
+    gene_counts <- melt(genes_from_time_course_filt[word,])$"value"  -4.5 #value added to match patterns on y-axis
+    start_table["counts"] <- gene_counts
+    start_table["gene_ID"] <- c(rep(word,length(gene_counts)))
+    start_table["type"] <- c(rep("norm_counts",length(gene_counts)))
+    start_table["gene"] <- c(rep(word,length(gene_counts)))
+    datalist[[word]] <- start_table
+    
+    start_table <- sample_table_short
+    smooth_3_A <- melt(smooth_short_A_filt[word,])$"value"
+    smooth_3_B <- melt(smooth_short_B_filt[word,])$"value"
+    start_table["counts"] <- c(smooth_3_A,smooth_3_B)
+    start_table["gene_ID"] <- c(rep(paste(word,"_smooth",sep=""),length(gene_counts)))
+    start_table["type"] <- c(rep("48h_spline",length(gene_counts)))
+    start_table["gene"] <- c(rep(word,length(gene_counts)))
+    datalist[[paste(word,"_smooth",sep="")]] <- start_table
+    
+    start_table <- sample_table_short
+    smooth_1to7_A <- c(melt(smooth_short_1to7_A_filt[word,])$"value",rep(NA,10))
+    smooth_1to7_B <- c(melt(smooth_short_1to7_B_filt[word,])$"value",rep(NA,10))
+    start_table["counts"] <- c(smooth_1to7_A,smooth_1to7_B)
+    start_table["gene_ID"] <- c(rep(paste(word,"_smooth_1to7",sep=""),length(gene_counts)))
+    start_table["type"] <- c(rep("8h_spline",length(gene_counts)))
+    start_table["gene"] <- c(rep(word,length(gene_counts)))
+    datalist[[paste(word,"_smooth_1to7",sep="")]] <- start_table
+  }
+}
+
+big_data = do.call(rbind,datalist)
+
+big_data['group'] <- paste(big_data$gene_ID,big_data$replicate,sep="_")
+
+name = "../plots/Figure_2B_smoothing_short_Gale-Galk.pdf"
+pdf(name,width=7,height=3,paper="special")
+
+ggplot(big_data, aes(x=hours, y=counts, color=type, group=group)) +
+  scale_y_log10() + 
+  geom_point(size=2) + 
+  geom_line(size=1, aes(linetype=gene)) + 
+  theme(axis.text=element_text(size=12),
+        legend.text=element_text(size=12),
+        axis.text.y=element_blank(),
+        legend.title=element_text(size=12),
+        axis.title.x=element_text(size=12, margin=margin(5,0,0,0)),
+        axis.title.y = element_blank()) +
+  scale_x_continuous(breaks=c(0,6,12,24,36,48)) +
+  scale_color_manual(values=c("#5F80F3","#dbc300","#17CE6B")) +
+  #ylab("normalized counts") +
+  xlab("Hours after injection")
+
+dev.off()
+
+
+
+# Figure 2C) smoothing of long term patterns
+# example with AttA and DptB
+
+genes <- c("AttA","DptB")
+
+list_of_interest <- translation_file_filtered[translation_file_filtered$gene_symbol %in% genes,]$geneID
+type = 'name'
+
+genes_from_time_course <- dat_norm_short # choose if dat_norm or dat_norm_short
+start_table <- sample_table_short # choose if sample_table or sample_table_short
+
+genes_from_time_course_filt <- genes_from_time_course[list_of_interest,]
+smooth_short_A_filt <- smooth_short_A[list_of_interest,]
+smooth_short_B_filt <- smooth_short_B[list_of_interest,]
+smooth_short_1to7_A_filt <- smooth_short_1to7_A[list_of_interest,]
+smooth_short_1to7_B_filt <- smooth_short_1to7_B[list_of_interest,]
+
+rownames(genes_from_time_course_filt) <- as.character(plyr::mapvalues(x=rownames(genes_from_time_course_filt),
+                                                                      from=translation_file_filtered$geneID,
+                                                                      to=translation_file_filtered$gene_symbol,
+                                                                      warn_missing = F))
+rownames(smooth_short_A_filt) <- as.character(plyr::mapvalues(x=rownames(smooth_short_A_filt),
+                                                              from=translation_file_filtered$geneID,
+                                                              to=translation_file_filtered$gene_symbol,
+                                                              warn_missing = F))
+rownames(smooth_short_B_filt) <- as.character(plyr::mapvalues(x=rownames(smooth_short_B_filt),
+                                                              from=translation_file_filtered$geneID,
+                                                              to=translation_file_filtered$gene_symbol,
+                                                              warn_missing = F))
+rownames(smooth_short_1to7_A_filt) <- as.character(plyr::mapvalues(x=rownames(smooth_short_1to7_A_filt),
+                                                                   from=translation_file_filtered$geneID,
+                                                                   to=translation_file_filtered$gene_symbol,
+                                                                   warn_missing = F))
+rownames(smooth_short_1to7_B_filt) <- as.character(plyr::mapvalues(x=rownames(smooth_short_1to7_B_filt),
+                                                                   from=translation_file_filtered$geneID,
+                                                                   to=translation_file_filtered$gene_symbol,
+                                                                   warn_missing = F))
+
+
+datalist = list()
+
+for(word in genes){
+  if(word %in% rownames(genes_from_time_course_filt)){
+    gene_counts <- melt(genes_from_time_course_filt[word,])$"value"  -4.5 #value added to match patterns on y-axis
+    start_table["counts"] <- gene_counts
+    start_table["gene_ID"] <- c(rep(word,length(gene_counts)))
+    start_table["type"] <- c(rep("norm_counts",length(gene_counts)))
+    start_table["gene"] <- c(rep(word,length(gene_counts)))
+    datalist[[word]] <- start_table
+    
+    start_table <- sample_table_short
+    smooth_3_A <- melt(smooth_short_A_filt[word,])$"value"
+    smooth_3_B <- melt(smooth_short_B_filt[word,])$"value"
+    start_table["counts"] <- c(smooth_3_A,smooth_3_B)
+    start_table["gene_ID"] <- c(rep(paste(word,"_smooth",sep=""),length(gene_counts)))
+    start_table["type"] <- c(rep("48h_spline",length(gene_counts)))
+    start_table["gene"] <- c(rep(word,length(gene_counts)))
+    datalist[[paste(word,"_smooth",sep="")]] <- start_table
+    
+    start_table <- sample_table_short
+    smooth_1to7_A <- c(melt(smooth_short_1to7_A_filt[word,])$"value",rep(NA,10))
+    smooth_1to7_B <- c(melt(smooth_short_1to7_B_filt[word,])$"value",rep(NA,10))
+    start_table["counts"] <- c(smooth_1to7_A,smooth_1to7_B)
+    start_table["gene_ID"] <- c(rep(paste(word,"_smooth_1to7",sep=""),length(gene_counts)))
+    start_table["type"] <- c(rep("8h_spline",length(gene_counts)))
+    start_table["gene"] <- c(rep(word,length(gene_counts)))
+    datalist[[paste(word,"_smooth_1to7",sep="")]] <- start_table
+  }
+}
+
+big_data = do.call(rbind,datalist)
+
+big_data['group'] <- paste(big_data$gene_ID,big_data$replicate,sep="_")
+
+name = "../plots/Figure_2C_smoothing_long_AttA-DptB.pdf"
+pdf(name,width=7,height=3,paper="special")
+
+ggplot(big_data, aes(x=hours, y=counts, color=type, group=group)) +
+  scale_y_log10() + 
+  geom_point(size=2) + 
+  geom_line(size=1, aes(linetype=gene)) + 
+  theme(axis.text=element_text(size=12),
+        legend.text=element_text(size=12),
+        axis.text.y=element_blank(),
+        legend.title=element_text(size=12),
+        axis.title.x=element_text(size=12, margin=margin(5,0,0,0)),
+        axis.title.y = element_blank()) +
+  scale_x_continuous(breaks=c(0,6,12,24,36,48)) +
+  scale_color_manual(values=c("#5F80F3","#dbc300","#17CE6B")) +
+  #ylab("normalized counts") +
+  xlab("Hours after injection")
+
+dev.off()
+
---
+++ scripts/plotting_figure3.R
@@ -0,0 +1,119 @@
+# Scripts to generate plots in Figure 3
+# last updated: March 4th, 2021
+# Florencia Schlamp
+# mfs97@cornell.edu
+
+# Figure 3A) heatmap top 91 core DE genes
+
+library(pheatmap)
+library(RColorBrewer)
+library(dendsort)
+
+all_logFCs <- read.csv("../results/log_fold_change_short.filtered_ALL_spline_3.csv", row.names = 1)
+load("../results/gene_lists/pairwiseDE_pval0.01_logFC2_2tp_91genes.rdata")
+subset_logFCs <- all_logFCs[core_91,]
+mat <- as.matrix(subset_logFCs)
+dat <- data.frame(values = as.numeric(mat))
+colnames(mat) <- c(1,2,4,5,6,8,10,12,14,16,20,24,30,36,42,48)
+
+load("../data/translate_geneID_genename.rdata")
+
+rownames(mat) <- as.character(plyr::mapvalues(x=rownames(mat),
+                                              from=translation_file_filtered$geneID,
+                                              to=translation_file_filtered$gene_symbol,
+                                              warn_missing = F))
+
+mat_breaks <- seq(-max(mat), max(mat), length.out = 12)
+
+sort_hclust <- function(...) as.hclust(dendsort(as.dendrogram(...)))
+mat_cluster_rows <- sort_hclust(hclust(dist((mat))))
+
+name = "../plots/Figure_3A_core91_heatmap.pdf"
+pdf(name,width=6.6,height=10,paper="special")
+
+pheatmap(mat = mat,
+         color  = rev(brewer.pal(length(mat_breaks)-1, "PuOr")),
+         breaks = mat_breaks,
+         border_color = NA,
+         show_colnames= T,
+         show_rownames= T,
+         cluster_rows = mat_cluster_rows,
+         cluster_cols = F,
+         drop_levels  = TRUE,
+         fontsize     = 12,
+         fontsize_row = 8,
+         fontsize_col = 12,
+         angle_col = 0)
+
+dev.off()
+
+
+
+
+# Figure 4B) plot genes up and down and immune bar plot
+
+log_fold_change_short_f <- read.csv("../results/log_fold_change_short.filtered_pvalue-cut_0.01_logFC-cut_2_tp-cut_2_spline_3.csv", row.names = 1)
+nrow(log_fold_change_short_f)
+
+immune_genes <- as.character(read.csv("../data/List_of_immune_genes_updated.csv")$FlyBaseID)
+# note that this file still had old gene names, using flybaseID instead
+length(immune_genes)
+
+
+summary_table <- matrix(ncol=3, nrow=16)
+colnames(summary_table) <- c(' up-regulated   ',' down-regulated',' up-regulated immune genes   ')
+rownames(summary_table) <- c("1","2","4","5","6","8","10","12","14","16",
+                             "20","24","30","36","42","48")
+
+for(timepoint in 1:16){
+  summary_table[,' down-regulated'][timepoint] <- -length(log_fold_change_short_f[timepoint][log_fold_change_short_f[timepoint] < 0])
+  
+  #  summary_table[,'down-regulated immune genes'][timepoint] <- -length(log_fold_change_short_f[timepoint][log_fold_change_short_f[timepoint] < 0 & rownames(log_fold_change_short_f[timepoint]) %in% immune_genes])
+  
+  summary_table[,' up-regulated immune genes   '][timepoint] <- length(log_fold_change_short_f[timepoint][log_fold_change_short_f[timepoint] > 0 & rownames(log_fold_change_short_f[timepoint]) %in% immune_genes])
+  
+  summary_table[,' up-regulated   '][timepoint] <- length(log_fold_change_short_f[timepoint][log_fold_change_short_f[timepoint] > 0]) - length(log_fold_change_short_f[timepoint][log_fold_change_short_f[timepoint] > 0 & rownames(log_fold_change_short_f[timepoint]) %in% immune_genes])
+}
+
+melted_table <- melt(summary_table)
+melted_table$Var1 <- factor(melted_table$Var1, levels = c("1","2","4","5","6","8","10","12","14","16",
+                                                          "20","24","30","36","42","48"))
+melted_table$labels <- abs(melted_table$value)
+colnames(melted_table) <- c("Var1","Genes","value","labels")
+
+melted_table$labels[melted_table$Genes == ' up-regulated   '] <- melted_table$value[melted_table$Genes == ' up-regulated   '] + melted_table$value[melted_table$Genes == ' up-regulated immune genes   ']
+
+
+melted_table$Genes <- factor(melted_table$Genes, levels = c(" up-regulated   ",
+                                                            " up-regulated immune genes   ",
+                                                            " down-regulated"))
+
+name = "../plots/Figure_3B_genedistribution.pdf"
+pdf(name,width=9,height=4.5,paper="special")
+
+ggplot(melted_table, aes(x=Var1, y=value, fill=Genes)) + 
+  geom_bar(position="stack",width=0.75,stat="identity",color='black',size=0.1) +
+  ylab("Number of Genes") +
+  xlab("Hours after injection") +
+  geom_text(data=melted_table[melted_table$Genes == ' up-regulated immune genes   ',],
+            aes(label=melted_table[melted_table$Genes == ' up-regulated immune genes   ',]$labels), 
+            vjust=-0.3, size=3.5) +
+  geom_text(data=melted_table[melted_table$Genes == ' up-regulated   ',],
+            aes(y=labels, label=melted_table[melted_table$Genes == ' up-regulated   ',]$labels), 
+            vjust=-0.3, size=3.5) +
+  geom_text(data=melted_table[melted_table$Genes == ' down-regulated',],
+            aes(label=melted_table[melted_table$Genes == ' down-regulated',]$labels), 
+            vjust=1.2, size=3.5) +
+  theme_classic() +
+  theme(axis.text=element_text(size=12),
+        legend.text=element_text(size=12),
+        legend.title=element_blank(),
+        axis.title.x=element_text(size=12, margin=margin(7,0,0,0)),
+        axis.title.y=element_text(size=12, margin=margin(0,5,0,0)),
+        legend.position = "bottom") +
+  geom_abline(slope=0, color="black") +
+  scale_y_continuous(breaks=c(-10,0,10,30,60)) +
+  scale_fill_manual(values=c("#F8766D", "#F8766D", "#619CFF"))
+
+dev.off()
+
